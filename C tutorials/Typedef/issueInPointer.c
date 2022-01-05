aec4dd1030a54e6a90f8e6d21ecd2bad14a552d8
ErMapsh <maheshmestri73@gmail.com>
2022-01-05 10:19:04 +0530

    learn about typedef and how to solve issue in pointer while declaration

---
+++ C tutorials/Typedef/issueInPointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    // Here error throwing
+    int* a, b; // here pointer is assign only to a to not b.
+    int c = 87;
+    a = &c;
+    b = &c;// he is not pointer thats why here we got error. 
+
+    printf("%d\n", *(a));
+    printf("%p\n", *(b));
+    */
+
+    // solution on pointer
+    typedef int* integerPointer;
+    integerPointer a, b;
+    int c = 78;
+    a = &c;
+    b = &c;
+    // here we got right things
+    printf("%d\n", *(a));
+    printf("%d\n", *(b));
+    return 0;
+}
