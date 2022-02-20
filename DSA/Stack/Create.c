b7ea866bbfa86775cc4c3ddf2545c40f6c1b8df5
ErMapsh <maheshmestri73@gmaail.com>
2022-02-20 17:02:15 +0530

    stack operation on stack

---
+++ DSA/Stack/Create.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    // struct stack s;
+    // s.size = 80;
+    // s.top = -1;
+    // s.arr = (int *) malloc(s.size * sizeof(int));
+
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+
+    if (isEmpty(s))
+    {
+        printf("Stack is Empty\n");
+    }
+    else
+    {
+        printf("Stack is not Empty\n");
+    }
+
+    if (isFull(s))
+    {
+        printf("Stack is full\n");
+    }
+    else
+    {
+        printf("Stack is Not full\n");
+    }
+    return 0;
+}
