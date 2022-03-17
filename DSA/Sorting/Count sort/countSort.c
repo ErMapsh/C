57bff5ce3ac67fb08ddc5d0493de4e984b376789
ErMapsh <maheshmestri73@gmail.com>
2022-03-17 10:02:45 +0530

    linked representation of binary tree

--- DSA/Sorting/Count sort/countSort.c
+++ DSA/Sorting/Count sort/countSort.c
@@ -10,7 +10,7 @@ int FindMax(int *arr, int n)
             max = arr[i];
         }
     }
-    return max+1;
+    return max + 1;
 }
 
 void printArray(int *A, int n)
@@ -22,56 +22,54 @@ void printArray(int *A, int n)
     printf("\n");
 }
 
-void CountSort(int *A, int n){
+void CountSort(int *A, int n)
+{
     int Max = FindMax(A, n);
     // printf("%d\n", Max);
 
     // ---------Created new extra array and initialize with all blocks with value 0----------
-    int *AuxiArr = (int *) malloc(Max * sizeof(int));
+    int *AuxiArr = (int *)malloc(Max * sizeof(int));
     for (int i = 0; i < Max; i++)
-    {   
-         AuxiArr[i] = 0;
+    {
+        AuxiArr[i] = 0;
     }
     // printArray(AuxiArr, Max); // To do for his happen or not
 
-    // -----travasing and increamenting value by 1--
+    // -----travasing and increamenting value by 1-----------
     for (int j = 0; j < n; j++)
     {
         if (A[j])
         {
             AuxiArr[A[j]]++;
         }
-        
     }
     // printArray(AuxiArr, Max);
 
-    // time for sorting data
-    int p = 0;// p is for stroing index at A (in given array)
+    // --------------time for sorting data in main array-----------
+    int p = 0; // p is for stroing index at A (in given array)
     for (int k = 0; k < Max;)
     {
-        if (AuxiArr[k] == 0 )
+        if (AuxiArr[k] == 0)
         {
-            k++;
-        }else{
+            k++; // if value in auxi array is 0 then go for next index
+        }
+        else
+        {
+            // if not then push the index value in Main value and increament main array pointer
             A[p] = k;
             p++;
-            AuxiArr[k]--;
-        } 
+            AuxiArr[k]--; // and decreament value by 1
+        }
     }
-
     // printArray(A, n);
-
-    
-
+    free(AuxiArr);
 }
 
 int main(int argc, char const *argv[])
 {
     int Array[] = {3, 1, 9, 7, 1, 2, 4};
     int sizeOfArr = sizeof(Array) / sizeof(Array[0]);
-    
 
-    
     printf("Before Sorting\n");
     printArray(Array, sizeOfArr);
     printf("After Sorting\n");
