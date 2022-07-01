c6acd1195aad300abb0c334fc848619d623aa4ae
ErMapsh <maheshmestri73@gmail.com>
2022-07-01 21:19:59 +0530

    what is template in cpp

---
+++ C++/Tutorials/Competative Programming/1template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class vector
+{
+    T *arr;
+    int size;
+
+public:
+    vector(){}
+
+    vector(T *x){
+        // steps
+    }
+    // and many other methods
+};
+
+int main()
+{
+    vector<int> myVec1;
+    vector<float> myVec2;
+    return 0;
+}
