98a760d7e6f35d40e9fe354266ad976a739eb798
ErMapsh <maheshmestri73@gmail.com>
2022-07-01 14:18:23 +0530

    basic read write with files

---
+++ C++/Tutorials/File Input Output/fileIO.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+/*
+    while working with file in c++, we need a header file "fstream".
+    fstream header file having classes for reading or writing operation.
+
+    These are some useful classes for working with files in C++
+        fstreambase
+        ifstream --> derived from fstreambase
+        ofstream --> derived from fstreambas
+
+    In order to work with files in C++, you will have to open it. Primarily,
+    there are 2 ways to open a file:
+        Using the constructor
+        Using the member function open() of the class
+*/
+
+int main(int argc, char const *argv[])
+{
+    // opening file using constructer and perform write operation
+    string s1 = "in these file, there are data of file name sample1.txt";
+    ofstream out("sample1.txt"); // ofstream is a class-->output-fstream
+    out<<s1;// out object main string insert krdo, insertion operater se
+
+    // opening file using constructer and perform read operation
+    string s2;
+    ifstream in("sample2.txt"); // all data comming in in object;
+    in>>s2; 
+    cout<<s2;
+    getline(in, s2);
+    cout<<s2;
+    return 0;
+}
