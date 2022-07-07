b7b7080e861f95f664ad8ec8ee91622ae30ce91b
ErMapsh <maheshmestri73@gmail.com>
2022-07-07 14:52:11 +0530

    abstract data type (ADT) using array

--- DSA/ARRAY/ARRAY_ADT/1.c
+++ DSA/ARRAY/ARRAY_ADT/1.c
@@ -12,14 +12,15 @@ struct MyArray
 
 void CreateArray(struct MyArray *StructPointer, int tSize, int uSize)
 {
-    // printf("%d", (*StructPointer)); // this is base address of our array or structure
-    // (*StructPointer).total_size = tSize;
-    // (*StructPointer).used_size = uSize;
-    // (*StructPointer).ptr = (int *)malloc(tSize * sizeof(int)); // here create array in heap
-
+    /* 
+     printf("%d", (*StructPointer)); // this is base address of our array or structure
+     (*StructPointer).total_size = tSize;
+     (*StructPointer).used_size = uSize;
+     (*StructPointer).ptr = (int *)malloc(tSize * sizeof(int)); // here create array in heap
+    */
     StructPointer->total_size = tSize;
     StructPointer->used_size = uSize;
-    StructPointer->ptr = (int *)malloc(tSize * sizeof(int)); // here create array in heap
+    StructPointer->ptr = (int *)malloc(StructPointer->total_size * sizeof(int)); // here create array in heap
 }
 
 void SetValue(struct MyArray *stuctPtr)
@@ -43,14 +44,14 @@ void Show(struct MyArray *stuctPtr)
 
 int main(int argc, char const *argv[])
 {
-    struct MyArray Marks;       // instance of MyArray
+    struct MyArray Marks; // instance of MyArray
     printf("Creating Array..............\nDone...\n");
 
     CreateArray(&Marks, 10, 3); // here we pass Our Structure pointer, total student size and used size of student
 
     printf("We Are Running to get marks of reserved student....\n");
     SetValue(&Marks);
-    
+
     printf("\nAnd Display all marks of students: \n");
     Show(&Marks);
     return 0;
